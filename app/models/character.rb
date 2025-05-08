a3af671f38f1ed32738cb39dacd45f40e137bfa7
Ana <anaclimgo@gmail.com>
2025-05-08 19:10:36 -0300

    feat: character specifics

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,7 @@
 class Character < ApplicationRecord
+  has_many :character_items
+  has_many :items, through: :character_items
+
+  validates :name, presence: true
+  valdates :health, numericality: { greater_than_or_equal_to: 0}
 end
