cd535dc6f8cdd60928736c9fab39be10fc8f6153
Ana <anaclimgo@gmail.com>
2025-05-09 15:13:52 -0300

    feat: update game with actions logic

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -10,4 +10,18 @@ class GameController < ApplicationController
     @items = @character.items
     session[:current_scenario] = @scenario.includes
   end
+
+  def update
+    action = Action.find(params[:action_id])
+    roll = rand(1..20) + item_bonus
+    success = roll >= action.difficulty
+
+    if success
+      handle_success(action)
+    else
+      handle_failure(action)
+    end
+
+    redirect_to game_over_path unless @character.alive
+  end
 end
